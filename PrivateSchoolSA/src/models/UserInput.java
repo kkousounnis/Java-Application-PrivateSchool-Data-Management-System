949af3c26b174c9f4d112c9c15e7062bb7128bc5
root <root@konstantinos-Alienware-15-R2>
2020-11-15 02:34:06 +0200

    Private SchoolSA Project, add user interface and controllers

--- PrivateSchoolSA/src/models/UserInput.java
+++ PrivateSchoolSA/src/models/UserInput.java
@@ -46,21 +46,37 @@ public class UserInput {
     public static void manualCourseList() {
         Scanner sc = new Scanner(System.in);
         char tmp;
-        System.out.println("Please give the title of course.");
-        TitleName t1 = new TitleName(sc.next());
-        Course c = new Course(t1);
-        System.out.println("Please give the name of course");
-        c.setStream(sc.next());
-        do {
-            System.out.println("Please type (f) if it is FullTime"
-                    + " or type (p) if it is PartTime");
-            tmp = sc.next().charAt(0);
-        } while (checkInputType(tmp) == false);
-        c.setType(checkPartFullTime(tmp));
-        System.out.println("Please give me the start date of the course");
-        c.setStartDate(ConvertDateLong.convertDate(sc.next()));
-        System.out.println("Please give me the ending date of the course");
-        c.setEndDate(ConvertDateLong.convertDate(sc.next()));
+        TitleName t1;
+        Course c;
+        if (multipleOrStepBystepInput()) {
+            System.out.println("Please give the title of course.");
+            t1 = new TitleName(sc.next());
+            c = new Course(t1);
+            System.out.println("Please give the name of course");
+            c.setStream(sc.next());
+            do {
+                System.out.println("Please type (f) if it is FullTime"
+                        + " or type (p) if it is PartTime");
+                tmp = sc.next().charAt(0);
+            } while (checkInputType(tmp) == false);
+            c.setType(checkPartFullTime(tmp));
+            System.out.println("Please give me the start date of the course");
+            c.setStartDate(ConvertDateLong.convertDate(sc.next()));
+            System.out.println("Please give me the ending date of the course");
+            c.setEndDate(ConvertDateLong.convertDate(sc.next()));
+        } else {
+            System.out.println("[coursetitle stream (f)/(p) "
+                    + "startdate enddate]");
+            String allInputs = sc.nextLine();
+            String[] student = allInputs.split(" ");
+            t1 = new TitleName(student[0]);
+            c = new Course(t1);
+            c.setStream(student[1]);
+            c.setType(checkPartFullTime(student[2].charAt(0)));
+            c.setStartDate(ConvertDateLong.convertDate(student[3]));
+            c.setEndDate(ConvertDateLong.convertDate(student[4]));
+        }
+
         AddDataLists.AddCourseList(c);
 
     }
@@ -68,13 +84,26 @@ public class UserInput {
     public static void manualTrainer() {
         Scanner sc = new Scanner(System.in);
         Person p = new Person();
-        System.out.println("Please give me trainers first name.");
-        p.setFirstName(sc.next());
-        System.out.println("Please give me trainers last name.");
-        p.setLastName(sc.next());
-        Trainer t = new Trainer(p.getFirstName(), p.getLastName());
-        System.out.println("Please give me what subject the trainer teaches");
-        t.setSubject(sc.next());
+        Trainer t;
+        if (multipleOrStepBystepInput()) {
+
+            System.out.println("Please give me trainers first name.");
+            p.setFirstName(sc.next());
+            System.out.println("Please give me trainers last name.");
+            p.setLastName(sc.next());
+            t = new Trainer(p.getFirstName(), p.getLastName());
+            System.out.println("Please give me what subject the trainer teaches");
+            t.setSubject(sc.next());
+        } else {
+            System.out.println("[firstname lastname subject]");
+            String allInputs = sc.nextLine();
+            String[] student = allInputs.split(" ");
+            p.setFirstName(student[0]);
+            p.setLastName(student[1]);
+            t = new Trainer(p.getFirstName(), p.getLastName());
+            t.setSubject(student[2]);
+
+        }
         AddDataLists.AddTrainer(t);
         ControllerData.showCourses();
         System.out.println("Please tell me which course"
@@ -84,16 +113,33 @@ public class UserInput {
 
     public static void manualAssignment() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please give me the title of assignment");
-        TitleName title1 = new TitleName(sc.next());
+        TitleName title1;
+        Assignment a;
+        if (multipleOrStepBystepInput()) {
+            System.out.println("Please give me the title of assignment");
+            title1 = new TitleName(sc.next());
 
-        Assignment a = new Assignment(title1);
-        System.out.println("Please give me a description of assignment");
-        sc.nextLine();
-        a.setDescription(sc.nextLine());
-        System.out.println("Please give me a date to assign ");
-        a.setSubDateTime(ConvertDateLong.convertDate(sc.next()));
+            a = new Assignment(title1);
+            System.out.println("Please give me a description of assignment");
+            sc.nextLine();
+            a.setDescription(sc.nextLine());
+            System.out.println("Please give me a date to assign ");
+            a.setSubDateTime(ConvertDateLong.convertDate(sc.next()));
+        } else {
+            System.out.println("[assignmenttitle assignmentdate description ]");
+            String allInputs = sc.nextLine();
+            String[] student = allInputs.split(" ");
+            title1 = new TitleName(student[0]);
+            a = new Assignment(title1);
+            a.setSubDateTime(ConvertDateLong.convertDate(student[1]));
+           String s="";
+           for (int i = 2; i < student.length; i++) {
+               s=s+" "+student[i];
+           }
+           
+            a.setDescription(s);
 
+        }
         AddDataLists.AddAssignment(a);
         ControllerData.showCourses();
         System.out.println("Please tell to which course assignment belongs");
